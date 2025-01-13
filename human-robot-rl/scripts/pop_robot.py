2c759ef6416df74e8003d4b4d7d0667f212e34fd
madjel <madjel@laas.fr>
2025-01-13 22:36:22 +0100

    feat(simulation): adding pop robot in simulation env.

---
+++ human-robot-rl/scripts/pop_robot.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+import genesis as gs
+
+########################## init ##########################
+gs.init(backend=gs.gpu)
+
+
+########################## create a scene ##########################
+scene = gs.Scene(
+    viewer_options = gs.options.ViewerOptions(
+        camera_pos    = (0, -3.5, 2.5),
+        camera_lookat = (0.0, 0.0, 0.5),
+        camera_fov    = 30,
+        res           = (960, 640),
+        max_FPS       = 60,
+    ),
+    sim_options = gs.options.SimOptions(
+        dt = 0.01,
+    ),
+    show_viewer = True,
+)
+
+########################## entities ##########################
+plane = scene.add_entity(
+    gs.morphs.Plane(),
+)
+robot = scene.add_entity(
+    gs.morphs.URDF(
+        file  = '../models/mate.urdf',
+        pos=(0.0, 0.3, 1.0),
+        quat=(1.0, 0.0, 0.0, 0.0),
+        fixed = True),
+)
+
+print("n_qs:", robot.n_qs)
+print("n_links:", robot.n_links)
+print("n_joints:", robot.n_joints)
+print("n_dofs:", robot.n_dofs)
+
+########################## build ##########################
+scene.build()
+
+# start camera recording. Once this is started, all the rgb images rendered will be recorded internally
+q = np.zeros(robot.n_qs)
+robot.set_qpos(q)
+
+k=0
+for i in range(10):
+    q = np.zeros(robot.n_qs)
+    robot.set_qpos(q)
+
+    scene.step()
+    k=i
+
+for i in range(100):
+    # cam.render()
+    scene.step()
+
+import numpy as np
+for i in range(350):
+    scene.step()
